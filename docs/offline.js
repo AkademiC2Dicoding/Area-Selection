7dda5a428c43e0d0ac8b3421970911742c87bac9
puntodamar <punto.damar2@gmail.com>
2019-06-17 15:44:56 +0700

    game

---
+++ docs/offline.js
@@ -0,0 +1,21 @@
+﻿{
+	"version": 1560761084,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/minion1-sheet0.png",
+		"images/minion1-sheet1.png",
+		"images/minion2-sheet0.png",
+		"images/minion3-sheet0.png",
+		"images/horizontalup-sheet0.png",
+		"images/verticalleft-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
